bbb53d42ddd3ac22bd7f28f2a09489dc2aed7b94
Albert Villanueva Carreras <albertvc10@gmail.com>
2014-10-10 11:06:10 +0200

    - Feature completed: add second slider to change between two images

--- ChangingImages/ViewController.m
+++ ChangingImages/ViewController.m
@@ -3,6 +3,7 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UISlider *selectImageSlider;
 @property (weak, nonatomic) IBOutlet UIImageView *fadingImage;
 
 @end
@@ -19,7 +20,25 @@
     
     self.fadingImage.alpha = self.slider.value;
     
+}
+
+- (IBAction)changeImage:(id)sender {
     
+    UIImage *theImage;
+    
+    if (self.selectImageSlider.value > 50) {
+        theImage = [UIImage imageNamed:@"Fary.png"];
+        self.fadingImage.image = theImage;
+        
+        //Diferent ways
+        [self.fadingImage setImage:theImage];
+        [[self fadingImage]setImage:theImage];
+        [self fadingImage].image = theImage;
+    }
+    else {
+        theImage = [UIImage imageNamed:@"sadBatman.jpg"];
+        self.fadingImage.image = theImage;
+    }
 }
 
 @end
